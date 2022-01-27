cf4e2846cdb376af23f2684909237d01c4047d07
sitauc <sitauc@hotmail.com>
2022-01-27 14:30:32 -0500

    homework instrucciones 1

---
+++ 02-JS-I/homework/instrucciones.js
@@ -0,0 +1,5 @@
+Variables : la caja de juguetes con muchas cosas adentro
+ * Strings : cualquier texto encerrado en comillas
+ * Funciones (argumentos, `return`) permiten que te devuelvan una accion particular
+ * Declaraciones `if`: Si sucede algo o si cumple esa condicion
+ * Valores booleanos (`true`, `false`) : determinan si algo es verdadero o falso
